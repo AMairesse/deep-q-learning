e036d4c29bb4d5d7e35a443452ffdf3d48beed4b
Antoine Mairesse <antoine.mairesse@free.fr>
2022-11-04 12:10:04 +0100

    Update to gymnasium and use vectorized envs

--- ddqn.py
+++ ddqn.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import random
-import gym
+import gymnasium as gym
 import numpy as np
 from collections import deque
 from keras.models import Sequential
@@ -10,7 +10,7 @@ from keras import backend as K
 
 import tensorflow as tf
 
-EPISODES = 5000
+EPISODES = 5
 
 class DQNAgent:
     def __init__(self, state_size, action_size):
@@ -48,7 +48,7 @@ class DQNAgent:
         model.add(Dense(24, activation='relu'))
         model.add(Dense(self.action_size, activation='linear'))
         model.compile(loss=self._huber_loss,
-                      optimizer=Adam(lr=self.learning_rate))
+                      optimizer=Adam(learning_rate=self.learning_rate))
         return model
 
     def update_target_model(self):
@@ -56,25 +56,26 @@ class DQNAgent:
         self.target_model.set_weights(self.model.get_weights())
 
     def memorize(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
+        for i in range(state.shape[0]):
+            self.memory.append((state[i], action[i], reward[i], next_state[i], done[i]))
 
     def act(self, state):
         if np.random.rand() <= self.epsilon:
-            return random.randrange(self.action_size)
-        act_values = self.model.predict(state)
-        return np.argmax(act_values[0])  # returns action
+            return [random.randrange(self.action_size) for _ in range(state.shape[0])]
+        act_values = self.model.predict(state, verbose=0)
+        return np.argmax(act_values, axis=1)  # returns action
 
     def replay(self, batch_size):
         minibatch = random.sample(self.memory, batch_size)
         for state, action, reward, next_state, done in minibatch:
-            target = self.model.predict(state)
+            state = np.reshape(state, [1, state.shape[0]])
+            next_state = np.reshape(next_state, [1, next_state.shape[0]])
+            target = self.model.predict(state, verbose=0)
             if done:
                 target[0][action] = reward
             else:
-                # a = self.model.predict(next_state)[0]
-                t = self.target_model.predict(next_state)[0]
+                t = self.target_model.predict(next_state, verbose=0)[0]
                 target[0][action] = reward + self.gamma * np.amax(t)
-                # target[0][action] = reward + self.gamma * t[np.argmax(a)]
             self.model.fit(state, target, epochs=1, verbose=0)
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
@@ -87,31 +88,33 @@ class DQNAgent:
 
 
 if __name__ == "__main__":
-    env = gym.make('CartPole-v1')
-    state_size = env.observation_space.shape[0]
-    action_size = env.action_space.n
+    NUM_ENVS = 32
+
+    env = gym.vector.make('CartPole-v1', num_envs=NUM_ENVS)
+    state_size = env.single_observation_space.shape[0]
+    action_size = env.single_action_space.n
     agent = DQNAgent(state_size, action_size)
     # agent.load("./save/cartpole-ddqn.h5")
     done = False
     batch_size = 32
 
     for e in range(EPISODES):
-        state = env.reset()
-        state = np.reshape(state, [1, state_size])
+        state, info = env.reset()
+        state = np.reshape(state, [NUM_ENVS, state_size])
         for time in range(500):
             # env.render()
             action = agent.act(state)
-            next_state, reward, done, _ = env.step(action)
-            #reward = reward if not done else -10
-            x,x_dot,theta,theta_dot = next_state
-            r1 = (env.x_threshold - abs(x)) / env.x_threshold - 0.8
-            r2 = (env.theta_threshold_radians - abs(theta)) / env.theta_threshold_radians - 0.5
-            reward = r1 + r2
-            
-            next_state = np.reshape(next_state, [1, state_size])
-            agent.memorize(state, action, reward, next_state, done)
+            next_state, rewards, terminated, truncated, info = env.step(action)
+            done = np.logical_or(terminated, truncated)
+            rewards = [reward if not done[i] else -10 for i, reward in enumerate(rewards)]
+            #x,x_dot,theta,theta_dot = next_state
+            #r1 = (env.x_threshold - abs(x)) / env.x_threshold - 0.8
+            #r2 = (env.theta_threshold_radians - abs(theta)) / env.theta_threshold_radians - 0.5
+            #reward = r1 + r2            
+            next_state = np.reshape(next_state, [NUM_ENVS, state_size])
+            agent.memorize(state, action, rewards, next_state, done)
             state = next_state
-            if done:
+            if done.any():
                 agent.update_target_model()
                 print("episode: {}/{}, score: {}, e: {:.2}"
                       .format(e, EPISODES, time, agent.epsilon))
